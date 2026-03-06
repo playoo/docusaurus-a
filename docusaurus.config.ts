f471098551f4c6c720dd8f95329e445efd857fb1
playoo <98441484+playoo@users.noreply.github.com>
2026-03-06 09:17:35 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -173,7 +173,7 @@ const config: Config = {
         // },
       ],
       // 版权信息（动态获取当前年份）
-      copyright: `Copyright © ${new Date().getFullYear()} 1002工作室, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} 1002工作室. 使用 Docusaurus 构建.`,
     },
     // 代码高亮配置
     prism: {
